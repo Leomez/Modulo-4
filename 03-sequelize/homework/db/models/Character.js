3039bda94d1e518aa52c2a24d220ffb7e4a551c8
Leonardo Meza <leoariel17@gmail.com>
2022-10-21 15:58:17 -0300

    arrancando sequelize

--- 03-sequelize/homework/db/models/Character.js
+++ 03-sequelize/homework/db/models/Character.js
@@ -2,6 +2,24 @@ const { DataTypes } = require('sequelize');
 
 module.exports = sequelize => {
   sequelize.define('Character', {
-
+   code: {
+    type: DataTypes.STRING(5),
+    primaryKey: true
+   },
+   name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+   }
+   
   })
 }
+
+// code*: string (Máximo 5 caracteres) [PK]
+// name*: string (Debe ser único)
+// age: integer
+// race: enum (Posibles valores: 'Human', 'Elf', 'Machine', 'Demon', 'Animal', 'Other')
+// hp*: float
+// mana*: float
+// date_added: timestamp without time
+// En el caso de no setear una raza ("race") por default deberían asignarle "Other" y si no damos valor para "date_added" debería tomar la fecha actual. Adicionalmente queremos quitar los timestamps automáticos de createdAt y updatedAt.
